7d42c0970b7251c0313bd5fc5f4ffbb58e630028
Arbinda Yadav <arbind91117@gmail.com>
2025-10-23 07:41:05 +0545

    Update jobService.js

--- frontend/src/services/jobService.js
+++ frontend/src/services/jobService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/jobs",
+  baseURL: "https://job-portal-backend-96vc.onrender.com/api/jobs",
   withCredentials: true,
 });
 
